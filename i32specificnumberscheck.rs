aeea622c98d81394753e467babfe92a2fe664392
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-01 16:24:51 -0400

    Create i32specificnumberscheck.rs

---
+++ i32specificnumberscheck.rs
@@ -0,0 +1,18 @@
+fn test(theweirdnumber: i32) {
+  if theweirdnumber == 666 {
+    println!("CONSOLE.....");
+  }
+  if theweirdnumber == 333 {
+    println!("HALF OF THE NUMBER..");
+  }
+  if theweirdnumber == 123 {
+    println!("Nice!")
+  }
+  if theweirdnumber == 22222 {
+    println!("Yes this date happened, 2/2/2022");
+  }
+}
+
+fn main() {
+  test(22222);
+}
