04d92ba7d8e03a7349bf3e45fe2a06b7d5b26e38
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-01 16:36:23 -0400

    Create i32specificnumberscheck_withelse.rs

---
+++ i32specificnumberscheck_withelse.rs
@@ -0,0 +1,11 @@
+fn test(theweirdnumber: i32) {
+  if theweirdnumber == 500 {
+    println!("hi");
+  } else {
+    println!("Sorry, the number you inputted is not a number that has a custom print line.");
+  }
+}
+
+fn main() {
+  test(501);
+}
